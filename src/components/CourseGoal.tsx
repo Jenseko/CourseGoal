db3cc34e7fc12359ecff96be9663c6b161e3b2d7
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-10-01 16:36:55 +0200

    implement coursegoal component and adjust prettier, COURSEGOAL-001

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,20 @@
+type CourseGoalProps = {
+  title: string;
+  description: string;
+};
+
+export default function CourseGoal({ title, description }: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>
+          {title}
+        </h2>
+        <p>
+          {description}
+        </p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
