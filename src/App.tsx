c848e15cdaf1e7552bc21d48d2ae2f46dda7e1c7
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-10-02 10:27:55 +0200

    implement reactnode with using children prop, COURSEGOAL-002

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import CourseGoal from "./components/CourseGoal.tsx"
+import CourseGoal from "./components/CourseGoal.tsx";
 
-export default function App(){
-  return (<main>
-    <CourseGoal 
-      title='Learn React + TS' 
-      description='Learn it from stratch'/>
-  </main>
-  )
+export default function App() {
+  return (
+    <main>
+      <CourseGoal title="Learn React + TS">
+        <p>'Learn it from stratch'</p>
+      </CourseGoal>
+    </main>
+  );
 }
-
